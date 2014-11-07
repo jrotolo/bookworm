782cdbcc8e8b981775c653f86d4ffb960f8cf858
Jarrod <jrotolo91@gmail.com>
2014-11-07 23:38:04 +0000

    Made a basic user model and added a secure password hash

--- bookworm/config/routes.rb
+++ bookworm/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root             'static_pages#home'
   get 'about'  =>  'static_pages#about'
+  get 'signup' =>  'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
