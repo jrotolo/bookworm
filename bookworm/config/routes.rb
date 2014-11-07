39363baa5984c3f7846de5c87745892e2006ce3e
Jarrod <jrotolo91@gmail.com>
2014-11-07 23:18:44 +0000

    Static pages done, and bootstrapped

--- bookworm/config/routes.rb
+++ bookworm/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/about'
+  root 'static_pages#home'
+  get 'about'  =>  'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
