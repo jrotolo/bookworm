65ca8cc59a7df2776cd982564eba9dee6c349912
Jarrod <jrotolo91@gmail.com>
2014-11-08 19:35:47 +0000

    Added gravatar and fixed up profile page

--- bookworm/app/controllers/users_controller.rb
+++ bookworm/app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # debugger <- place this line anywhere in code to bring up debugger console in rails server pretty cool! See gem byebug.
   end
 end
