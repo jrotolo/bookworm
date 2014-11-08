069b74e30083ad642610ce9228a36634e85f62ee
Jarrod <jrotolo91@gmail.com>
2014-11-08 19:07:30 +0000

    Refactored user controller/working on profile pages

--- bookworm/config/routes.rb
+++ bookworm/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root             'static_pages#home'
   get 'about'  =>  'static_pages#about'
   get 'signup' =>  'users#new'
+  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
