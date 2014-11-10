92dc4910e5e5b121bd7509a2050e8114ce188a18
Jarrod <jrotolo91@gmail.com>
2014-11-10 06:54:22 +0000

    Login and authentication now work

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,19 @@
 module SessionsHelper
 
   # Logs the given user in
-  def log_in(user)
+  def login(user)
     # session creates a temporary cookie on the users browser and
     # stores the encrypted user id. Session expires on browser close.
     session[:user_id] = user.id
+  end
+
+  # Grabs the current user using user_id in session
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  # Test if a user is logged in
+  def logged_in?
+    !current_user.nil?
   end
 end
