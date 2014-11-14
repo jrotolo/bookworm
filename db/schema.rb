85bdb2b2d02ac2b0a808e5aaa8d648d4ebc3d734
jrotolo <jrotol5@lsu.edu>
2014-11-14 20:02:04 +0000

    Created relationship for following other users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107233406) do
+ActiveRecord::Schema.define(version: 20141114193854) do
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
