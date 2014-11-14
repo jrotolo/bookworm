c0bdaf9e49c8d4c18fe66c1bd4b3c951b6f46e1b
jrotolo <jrotol5@lsu.edu>
2014-11-14 20:07:41 +0000

    finished setting up database relations for followers and following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   has_many :active_relationships, class_name: "Relationship",
            :foreign_key: "follower_id", dependent: :destroy
-  has_many :following, through: :acrive_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+           :foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
