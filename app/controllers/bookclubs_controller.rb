60b478d87511280257c2a2339929f19676886f5a
jrotolo <jrotol5@lsu.edu>
2014-11-24 20:33:27 +0000

    OMG YES.. Its working.. Now time to clean it up

--- app/controllers/bookclubs_controller.rb
+++ app/controllers/bookclubs_controller.rb
@@ -1,5 +1,6 @@
 class BookclubsController < ApplicationController
   def new
+    @user = User.find(params[:user])
     @bookclub = Bookclub.new
   end
 
@@ -13,12 +14,12 @@ class BookclubsController < ApplicationController
   end
 
   def create
+    #@user = User.find(params[:user_id])
     @bookclub = Bookclub.new(bookclub_params)
-    @user = current_user
     if @bookclub.save
       @bookclub = Bookclub.new(:name => params[:bookclub][:name], 
-                               :description => params[:bookclub][:description], :user_id => params[:bookclub][:user_id])
-      redirect_to "/users/#{@user.id}/bookclubs"
+                               :description => params[:bookclub][:description], :user_id =>params[:bookclub][:user_id])
+      redirect_to "/users/#{@bookclub.user_id}/bookclubs"
     else
       render :action => 'new'
     end
--- app/models/bookclub.rb
+++ app/models/bookclub.rb
@@ -2,5 +2,6 @@ class Bookclub < ActiveRecord::Base
   belongs_to :user, :dependent => :destroy
   has_many :topics, :dependent => :destroy
   validates :name, presence: true, length: { maximum: 100 }
+  validates :user_id, presence: true
 end
 
